3c1359f77345af129c7b5853d05dadb131db109c
swer4412 <swer4412@gmail.com>
2023-07-25 22:35:44 +0200

    Sitemato routes ma non va tailwind :(

---
+++ Pages/Welcome.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+const Home: FC = () => {
+    return (
+        <div>
+            Benventuo nel mio sito caro stagista, in questa pagina puoi trovare diverse guide per imparare le basi del lavoro di tecnico di laboratorio
+        </div>
+    )
+}
+
+export default Home;
